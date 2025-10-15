829b619890c458d560687b0f5694a89a4f3ca0e2
Muhammad <usman.sharif@dynamologic.com>
2025-10-15 19:30:57 +0500

    Update CheckActiveUserController.php

--- app/Http/Controllers/API/CheckActiveUserController.php
+++ app/Http/Controllers/API/CheckActiveUserController.php
@@ -10,13 +10,13 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 class CheckActiveUserController extends Controller
 {
-     public function __construct()
+    public function __construct()
     {
         $this->middleware('auth:api');
     }
     
     protected function check_user(Request $request)
     {
-         return response(['result' => 'success'], 422);
+        return response(['result' => 'success']);
     }
 }
